4c9783e76f57f923d389839908fed50f5b959dad
Yaroslav <yibelyaev@icloud.com>
2023-12-11 17:20:45 +0300

    Правки

--- app/pages/HomePage/Gallery/Gallery.jsx
+++ app/pages/HomePage/Gallery/Gallery.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
-import cx from 'classnames'
+import cx from 'classnames';
 
-import {gallery} from "stubs/gallery";
+import { gallery } from 'stubs/gallery';
 
 import Title from 'components/Title';
 
@@ -9,15 +9,11 @@ import s from './Gallery.module.scss';
 
 const Gallery = () => {
   return (
-    <section className={s.gallery} id='#gallery'>
-      <div className={s.wrapperTitle}>
-        <Title
-          className={s.wrapper}
-          title="Фотографии путешествий"
-        />
-        <div
-          className={cx(s.list, s.list__first)}>
-          {gallery.slice(0,4).map((picture) =>
+    <section className={s.gallery} id="#gallery">
+      <div className={s.wrapper}>
+        <Title className={s.wrapperTitle} title="Фотографии путешествий" />
+        <div className={cx(s.list, s.list__first)}>
+          {gallery.slice(0, 4).map((picture) => (
             <Image
               key={picture.title}
               src={picture.img}
@@ -25,23 +21,21 @@ const Gallery = () => {
               width={442.5}
               height={301}
             />
-          )}
+          ))}
         </div>
-        <div
-          className={cx(s.list, s.list__second)}>
-          {gallery.slice(4, 9).map((picture) =>
-              <Image
-                key={picture.title}
-                src={picture.img}
-                alt={picture.title}
-                width={349.16}
-                height={301}
-              />
-            )}
+        <div className={cx(s.list, s.list__second)}>
+          {gallery.slice(4, 9).map((picture) => (
+            <Image
+              key={picture.title}
+              src={picture.img}
+              alt={picture.title}
+              width={349.16}
+              height={301}
+            />
+          ))}
         </div>
-        <div
-          className={cx(s.list, s.list__three)}>
-          {gallery.slice(9).map((picture) =>
+        <div className={cx(s.list, s.list__three)}>
+          {gallery.slice(9).map((picture) => (
             <Image
               key={picture.title}
               src={picture.img}
@@ -49,11 +43,11 @@ const Gallery = () => {
               width={442.5}
               height={301}
             />
-          )}
+          ))}
         </div>
       </div>
     </section>
   );
-}
+};
 
 export default Gallery;
